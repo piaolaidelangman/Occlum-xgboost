f8ecc779fa8d044ec288b299f573848d880b88dd
piaolaidelangman <707056129@qq.com>
2022-02-08 09:02:20 +0800

    User space size 4g

--- start-spark-local.sh
+++ start-spark-local.sh
@@ -10,6 +10,6 @@ sudo docker run -it \
 	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
     -v /home/sdp/diankun/process_data_10G:/opt/occlum_spark/data \
 	-e LOCAL_IP=192.168.0.111 \
-	-e SGX_MEM_SIZE=80GB \
+	-e SGX_MEM_SIZE=90GB \
 	xgboost-spark-sgx:2.0 \
 	bash /opt/run_spark_on_occlum_glibc.sh $1 && tail -f /dev/null
