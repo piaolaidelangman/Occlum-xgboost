5013793a124fd845e9fea828fac2bec44c27f6a9
piaolaidelangman <707056129@qq.com>
2022-02-08 22:05:19 +0800

    Xmxs64g

--- run_spark_on_occlum_glibc.sh
+++ run_spark_on_occlum_glibc.sh
@@ -11,16 +11,16 @@ init_instance() {
     # Init Occlum instance
     cd /opt
     # check if occlum_spark exists
+            # .resource_limits.user_space_size = "102400MB" |
+                    # .process.default_stack_size = "4096MB" |
     [[ -d occlum_spark ]] || mkdir occlum_spark
     cd occlum_spark
     occlum init
     new_json="$(jq '.resource_limits.user_space_size = "SGX_MEM_SIZE" |
         .resource_limits.max_num_of_threads = 4096 |
-        .resource_limits.user_space_size = "102400MB" |
         .process.default_heap_size = "4096MB" |
         .resource_limits.kernel_space_heap_size="4096MB" |
-        .process.default_mmap_size = "65536MB" |
-        .process.default_stack_size = "4096MB" |
+        .process.default_mmap_size = "71680MB" |
         .entry_points = [ "/usr/lib/jvm/java-11-openjdk-amd64/bin" ] |
         .env.untrusted = [ "DMLC_TRACKER_URI", "SPARK_DRIVER_URL" ] |
         .env.default = [ "LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-amd64/lib/server:/usr/lib/jvm/java-11-openjdk-amd64/lib:/usr/lib/jvm/java-11-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/bin/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/bin","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/bin/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
@@ -100,7 +100,7 @@ run_spark_xgboost_train() {
                 -Divy.home="/tmp/.ivy" \
                 -Dos.name="Linux" \
                 -cp "$SPARK_HOME/conf/:$SPARK_HOME/jars/*:/bin/jars/*" \
-                -Xms56g org.apache.spark.deploy.SparkSubmit \
+                -Xmx64g -Xms64g org.apache.spark.deploy.SparkSubmit \
                 --master local[16] \
                 --conf spark.task.cpus=16 \
                 --class occlumxgboost.xgbClassifierTrainingExample \
