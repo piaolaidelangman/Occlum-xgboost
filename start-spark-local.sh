0ce8fbdb63fd1c0c1cdc574520d3419652e67b55
piaolaidelangman <707056129@qq.com>
2022-02-11 21:40:26 +0800

    Last success. you should reset it. decrease 16g mmp size

--- start-spark-local.sh
+++ start-spark-local.sh
@@ -8,8 +8,8 @@ sudo docker run -it \
 	--device=/dev/sgx/enclave \
 	--device=/dev/sgx/provision \
 	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
-    -v /home/sdp/diankun/data/10G_data:/opt/occlum_spark/data \
+    -v /home/sdp/diankun/occlumData/:/opt/occlum_spark/data \
 	-e LOCAL_IP=192.168.0.111 \
-	-e SGX_MEM_SIZE=68GB \
+	-e SGX_MEM_SIZE=52GB \
 	xgboost-spark-sgx:2.0 \
 	bash /opt/run_spark_on_occlum_glibc.sh $1 && tail -f /dev/null
