787ea8b200cdb9bd23ccc02a377b8823699f5d33
piaolaidelangman <707056129@qq.com>
2022-02-12 07:41:40 +0800

    Last success. Test 62G epc

--- run_spark_on_occlum_glibc.sh
+++ run_spark_on_occlum_glibc.sh
@@ -100,7 +100,7 @@ run_spark_xgboost_train() {
                 -Divy.home="/tmp/.ivy" \
                 -Dos.name="Linux" \
                 -cp "$SPARK_HOME/conf/:$SPARK_HOME/jars/*:/bin/jars/*" \
-                -Xmx26g -Xms26g org.apache.spark.deploy.SparkSubmit \
+                -Xmx28g -Xms28g org.apache.spark.deploy.SparkSubmit \
                 --master local[16] \
                 --conf spark.task.cpus=8 \
                 --class occlumxgboost.xgbClassifierTrainingExample \
