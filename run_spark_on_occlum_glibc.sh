41055f5cbc6faf54f34da4b2c0f61f89f0256b24
piaolaidelangman <707056129@qq.com>
2022-02-11 13:23:14 +0800

    Last success. 30g epc

--- run_spark_on_occlum_glibc.sh
+++ run_spark_on_occlum_glibc.sh
@@ -18,9 +18,9 @@ init_instance() {
     occlum init
     new_json="$(jq '.resource_limits.user_space_size = "SGX_MEM_SIZE" |
         .resource_limits.max_num_of_threads = 4096 |
-        .process.default_heap_size = "16384MB" |
+        .process.default_heap_size = "10240MB" |
         .resource_limits.kernel_space_heap_size="4096MB" |
-        .process.default_mmap_size = "24576MB" |
+        .process.default_mmap_size = "20480MB" |
         .entry_points = [ "/usr/lib/jvm/java-11-openjdk-amd64/bin" ] |
         .env.untrusted = [ "DMLC_TRACKER_URI", "SPARK_DRIVER_URL" ] |
         .env.default = [ "LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-amd64/lib/server:/usr/lib/jvm/java-11-openjdk-amd64/lib:/usr/lib/jvm/java-11-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/bin/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/bin","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/bin/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
@@ -111,14 +111,7 @@ run_spark_xgboost_train() {
                 /bin/jars/xgboostsparksgx-1.0-SNAPSHOT-jar-with-dependencies.jar \
                 /host/data /host/data/model 8
 }
-                # --conf spark.scheduler.maxRegisteredResourcesWaitingTime=50000000 \
-                # --conf spark.worker.timeout=60000000 \
-                # --conf spark.network.timeout=10000000 \
-                # --conf spark.starvation.timeout=2500000 \
-                # --conf spark.speculation=false \
-                # --conf spark.executor.heartbeatInterval=10000000 \
-                # --conf spark.sql.shuffle.partitions=200 \
-                # --conf spark.shuffle.io.maxRetries=8 \
+
 id=$([ -f "$pid" ] && echo $(wc -l < "$pid") || echo "0")
 
 arg=$1
