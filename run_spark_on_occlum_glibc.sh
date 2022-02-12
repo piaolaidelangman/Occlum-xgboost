84d7c18a8aef937b6023f5fd76f2549293792f6b
piaolaidelangman <707056129@qq.com>
2022-02-12 19:55:07 +0800

    MaxMetaspaceSize

--- run_spark_on_occlum_glibc.sh
+++ run_spark_on_occlum_glibc.sh
@@ -20,7 +20,7 @@ init_instance() {
         .resource_limits.max_num_of_threads = 4096 |
         .resource_limits.kernel_space_heap_size="2048MB" |
         .process.default_heap_size = "32GB" |
-        .process.default_mmap_size = "32GB" |
+        .process.default_mmap_size = "36GB" |
         .entry_points = [ "/usr/lib/jvm/java-11-openjdk-amd64/bin" ] |
         .env.untrusted = [ "DMLC_TRACKER_URI", "SPARK_DRIVER_URL" ] |
         .env.default = [ "LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-amd64/lib/server:/usr/lib/jvm/java-11-openjdk-amd64/lib:/usr/lib/jvm/java-11-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/bin/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/bin","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/bin/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
@@ -95,7 +95,7 @@ run_spark_xgboost_train() {
     build_spark
     echo -e "occlum run xgboost spark "
     occlum run /usr/lib/jvm/java-11-openjdk-amd64/bin/java \
-                -XX:-UseCompressedOops -XX:MaxMetaspaceSize=1024m \
+                -XX:-UseCompressedOops -XX:MaxMetaspaceSize=256m \
                 -XX:ActiveProcessorCount=8 \
                 -Divy.home="/tmp/.ivy" \
                 -Dos.name="Linux" \
@@ -109,7 +109,7 @@ run_spark_xgboost_train() {
                 --executor-memory 2G \
                 --driver-memory 10G \
                 /bin/jars/xgboostsparksgx-1.0-SNAPSHOT-jar-with-dependencies.jar \
-                /host/data /host/data/model 2 10 2
+                /host/data /host/data/model 8 10 2
 }
 
 id=$([ -f "$pid" ] && echo $(wc -l < "$pid") || echo "0")
