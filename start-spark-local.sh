92ee2a4dba5fe5049ac3cfd5c55aba4e4465011d
piaolaidelangman <707056129@qq.com>
2022-02-12 20:00:25 +0800

    MaxMetaspaceSize

--- start-spark-local.sh
+++ start-spark-local.sh
@@ -10,7 +10,7 @@ sudo docker run -it \
 	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
     -v /home/sdp/diankun/occlumData:/opt/occlum_spark/data \
 	-e LOCAL_IP=192.168.0.111 \
-	-e SGX_MEM_SIZE=68GB \
+	-e SGX_MEM_SIZE=70GB \
 	xgboost-spark-sgx:2.0 \
 	bash /opt/run_spark_on_occlum_glibc.sh $1 && tail -f /dev/null
 
